6c0bcb0c55f2be871f4a792c4e74fca932ce96e3
shivansh8888 <shivanshjoshi41@gmail.com>
2026-03-18 14:05:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_chroma import Chroma
 from langchain_google_genai import ChatGoogleGenerativeAI, GoogleGenerativeAIEmbeddings
-from langchain.chains import create_retrieval_chain
-from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_classic.chains import create_retrieval_chain
+from langchain_classic.chains.combine_documents import create_stuff_documents_chain
 from langchain_core.prompts import ChatPromptTemplate
 
 # --- 1. PAGE SETUP ---
